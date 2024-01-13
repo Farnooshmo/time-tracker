c75ee675addce9ca3ec9913ec9d2142cc7aa89c6
elahemortazavi <106202329+elahemortazavi@users.noreply.github.com>
2024-01-13 15:55:43 +0000

    server run

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+
+
+app.listen(5001, () => {
+  console.log("server has started on port 5001");
+});
