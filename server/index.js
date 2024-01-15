0a81594d7bdb3f202d88c8f10f729a2ec03ea436
elahemortazavi <106202329+elahemortazavi@users.noreply.github.com>
2024-01-15 14:43:36 +0000

    components

--- server/index.js
+++ server/index.js
@@ -7,7 +7,20 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json()); //req.body
 
+//ROUTES//
 
+//create a todo
+
+
+//get all todos
+
+//get a todo
+
+
+//update a todo
+
+
+//delete a todo
 
 app.listen(5001, () => {
   console.log("server has started on port 5001");
