c6832c700474323cd188570092ee21c44b130b2b
Meysam-Arshadi <meysamarshadi@gmail.com>
2024-01-18 13:21:16 +0000

    meysam-creating-GET-request

--- server/db.js
+++ server/db.js
@@ -1,22 +1,22 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
+const { Pool } = require('pg')
+require('dotenv').config()
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-
-});
+	connectionString: process.env.DATABASE_URL,
+})
 
 async function getPostgresVersion() {
-  const client = await pool.connect();
-  try {
-    const response = await client.query('SELECT version()');
-    console.log(response.rows[0]);
-  } catch (error) {
-    console.error('Error executing query:', error);
-  } finally {
-    client.release();
-  }
+	const client = await pool.connect()
+	try {
+		const response = await client.query('SELECT version()')
+		console.log(response.rows[0])
+	} catch (error) {
+		console.error('Error executing query:', error)
+	} finally {
+		client.release()
+	}
 }
 
-getPostgresVersion();
+getPostgresVersion()
+
+module.exports = pool
