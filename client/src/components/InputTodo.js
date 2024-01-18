84a367f0c64d80bf8b67de641547c81fffe91ea5
elahemortazavi <106202329+elahemortazavi@users.noreply.github.com>
2024-01-18 19:01:23 +0000

    creating simple input-todo component

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,9 @@
+import React, { Fragment, useState } from "react";
+return (
+    <Fragment>
+      <h1 className="text-center mt-5">Pern Todo List</h1>
+     
+    </Fragment>
+  );
+
+export default InputTodo;
