033f5096e3e21059c61f0dc37e3978cb19d717bf
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-01-22 21:48:25 +0000

    Get a todo with ID

--- server/index.js
+++ server/index.js
@@ -27,18 +27,29 @@ app.post('/todos', async (req, res) => {
 //get all todos
 app.get('/todos', async (req, res) => {
 	try {
-		const allTodos = await pool.query('SELECT * FROM todo')
-		res.json(allTodos.rows)
+		const allTodos = await pool.query('SELECT * FROM todo');
+		res.json(allTodos.rows);
 	} catch (err) {
-		console.error(err.message)
+		console.error(err.message);
 	}
-})
+});
 
 //get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 //update a todo
 
 //delete a todo
+
+
 
 app.listen(5001, () => {
 	console.log('server has started on port 5001')
