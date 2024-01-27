eb8a7254a90351184e2f6dc30bc4220c66a4cf56
Meysam Arshadi <meysamarshadi@gmail.com>
2024-01-27 16:38:34 +0000

    meysam-clean-up-css

---
+++ client/src/components/StartTimer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import playerPlay from '../assets/playerPlay.svg'
+
+const StartTimer = () => {
+	return (
+		<div>
+			<img src={playerPlay} alt='Player Logo' />
+		</div>
+	)
+}
+
+export default StartTimer
