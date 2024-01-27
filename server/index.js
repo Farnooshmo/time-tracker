329035f3bf6038ab9a95875b41a2564f6aacf544
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2024-01-27 16:48:18 +0000

    Update index.js
    update functionality

--- server/index.js
+++ server/index.js
@@ -46,7 +46,19 @@ app.get('/todos/:id', async (req, res) => {
 });
 
 //update a todo
-
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2"
+    );
+  } catch (error) {
+    console.error(err.message);
+  }
+  
+});
+  
 //delete a todo
 app.delete('/todos/:id', async (req, res) => {
   try {
