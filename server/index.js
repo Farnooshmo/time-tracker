54ddad4f6db9491093f37d2219f44f3966cd9a51
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-02-01 00:02:01 +0000

    Debugging the update in backend

--- server/index.js
+++ server/index.js
@@ -51,10 +51,11 @@ app.put("/todos/:id", async (req, res) => {
     const { id } = req.params;
     const { description } = req.body;
     const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2"
+      "UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]
     );
+    res.json("Todo was updated")
   } catch (error) {
-    console.error(err.message);
+    console.error(error.message);
   }
   
 });
