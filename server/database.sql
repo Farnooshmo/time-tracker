27b8008a39c562fd7e68ceceef3cf3ef6b3225f4
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-02-03 14:27:09 +0000

    Create a table for time_tracking

--- server/database.sql
+++ server/database.sql
@@ -4,3 +4,12 @@ CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
+
+
+CREATE TABLE time_tracking (
+    tracking_id SERIAL PRIMARY KEY,
+    todo_id INT NOT NULL,
+    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    end_time TIMESTAMP,
+    FOREIGN KEY (todo_id) REFERENCES todo(todo_id)
+);
