b414c36ff15667cafe742be212d13477309bc218
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-02-03 20:35:54 +0000

    Create a table for tracking the time

--- server/database.sql
+++ server/database.sql
@@ -11,5 +11,8 @@ CREATE TABLE time_tracking (
     todo_id INT NOT NULL,
     start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     end_time TIMESTAMP,
+    duration NUMERIC, -- Duration of the time entry in seconds
+    total_time NUMERIC, -- Total time spent on the task in seconds
     FOREIGN KEY (todo_id) REFERENCES todo(todo_id)
 );
+
