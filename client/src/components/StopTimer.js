68088adf0b78b732fdd99ff8af2c9663497670cc
elahemortazavi <106202329+elahemortazavi@users.noreply.github.com>
2024-02-06 11:08:54 +0000

    start and stop functionality
    stop icon need to sort out

--- client/src/components/StopTimer.js
+++ client/src/components/StopTimer.js
@@ -1,10 +1,33 @@
-import React from "react";
-import playStop from "../assets/playerStop.svg";
+import React, { useState } from 'react';
+import playStop from '../assets/playerStop.svg';
 
-const StopTimer = () => {
-  return <div>
-    <img src= { playStop } alt= "Stop logo" className=""/>
-  </div>;
+const StopTimer = ({ onStop }) => {
+  const [isTimerRunning, setTimerRunning] = useState(false);
+
+  const handleStop = () => {
+    console.log('Stop button clicked'); // Debugging statement
+
+    // Update the timer running state
+    setTimerRunning(false);
+
+    // Trigger the stop callback
+    onStop();
+  };
+
+  return (
+    <div>
+      <img
+        src={playStop}
+        alt='Stop Logo'
+        onClick={handleStop}
+        style={{ cursor: 'pointer' }}
+      />
+    </div>
+  );
 };
 
 export default StopTimer;
+
+
+
+
