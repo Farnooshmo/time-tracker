030654d09f9b53487b446d11020eb8a33c0441c6
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-02-08 10:39:50 +0000

    Creating a new psql table

--- server/database.sql
+++ server/database.sql
@@ -1,18 +1,10 @@
+);
+
 CREATE DATABASE perntodo;
 
-CREATE TABLE todo(
+CREATE TABLE todo (
     todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    description VARCHAR(255),
+    start_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    end_time TIMESTAMP WITH TIME ZONE,
 );
-
-
-CREATE TABLE time_tracking (
-    tracking_id SERIAL PRIMARY KEY,
-    todo_id INT NOT NULL,
-    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    end_time TIMESTAMP,
-    duration NUMERIC, -- Duration of the time entry in seconds
-    total_time NUMERIC, -- Total time spent on the task in seconds
-    FOREIGN KEY (todo_id) REFERENCES todo(todo_id)
-);
-
