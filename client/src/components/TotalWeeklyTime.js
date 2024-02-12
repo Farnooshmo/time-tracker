39492eecffc7f46ce2ce8bee8944b3f45c35a119
elahemortazavi <106202329+elahemortazavi@users.noreply.github.com>
2024-02-12 12:33:52 +0000

    created daily, weekly, monthly components

---
+++ client/src/components/TotalWeeklyTime.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TotalWeeklyTime = ({ totalWeeklyTime }) => {
+  return (
+    <div>
+      <p>Weekly Activity: {totalWeeklyTime}</p>
+    </div>
+  );
+};
+
+export default TotalWeeklyTime;
