7be64c41b2f52cc5dfb914b994c1e169d8c99f31
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-03-09 10:04:10 +0000

    delete TasksTotalTime from SummaryActivities

--- client/src/components/SummaryActivities.js
+++ client/src/components/SummaryActivities.js
@@ -3,14 +3,12 @@ import '../App.css';
 import TotalDailyTime from './TotalDailyTime';
 import TotalWeeklyTime from './TotalWeeklyTime';
 import TotalMonthlyTime from './TotalMonthlyTime';
-// import TasksTotalTime from './TasksTotalTime';
 
 const SummaryActivities = () => {
 	return (
 		<div className='summary-box'>
 			<div className='daily-activity-section'>
 				<TotalDailyTime />
-				{/* <TasksTotalTime /> */}
 			</div>
 
 			<div className='weekly-activity-section'>
