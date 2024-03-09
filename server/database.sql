71ce96d8d0550bf1801b5aaae49ec486b0c7a5b9
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-03-09 09:51:30 +0000

    Delete TotalMonthlyTime table

--- server/database.sql
+++ server/database.sql
@@ -6,34 +6,8 @@ CREATE TABLE todo (
     description VARCHAR(255),
     start_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     end_time TIMESTAMP WITH TIME ZONE,
+    duration INTEGER,
+    total_time INTEGER,
+    today_activity INTEGER
 );
-ALTER TABLE todo
-ADD COLUMN duration INTEGER;
-ALTER TABLE todo
-ADD COLUMN total_time INTEGER;
-
-ALTER TABLE todo
-ADD COLUMN today_activity INTEGER;
-
-
-
-
-
-
-
-
-//totalMonthlyTime:
-
-CREATE TABLE TotalMonthlyTime (
-    id SERIAL PRIMARY KEY,
-    total_weekly_time_id INT REFERENCES TotalWeeklyTime(id) ON DELETE CASCADE,
-    month INT,
-    year INT,
-    monthly_total_time INTERVAL
-);
-
-
- id | total_weekly_time_id | month | year | monthly_total_time 
-----+----------------------+-------+------+--------------------
-(0 rows)
 
