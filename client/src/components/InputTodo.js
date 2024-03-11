364b717f452e9c38b9b572a827c540f31f097b54
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-03-11 22:27:39 +0000

    delete commented code

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -22,29 +22,6 @@ const InputTodo = ({ onTodoAdded }) => { // Pass a callback function to handle t
 		}
 	};
 
-
-
-	// const onSubmitForm = async (e) => {
-	// 	e.preventDefault();
-	// 	try {
-	// 		const body = {
-	// 			description,
-	// 			date: new Date().toISOString().split('T')[0], // Extract date and format it as YYYY-MM-DD
-	// 		};
-	// 		const response = await fetch('http://localhost:5001/todos', {
-	// 			method: 'POST',
-	// 			headers: { 'Content-Type': 'application/json' },
-	// 			body: JSON.stringify(body),
-	// 		});
-	// 		const data = await response.json();
-	// 		onTodoAdded(data); // Call the callback function to handle todo addition
-	// 		setDescription(''); // Reset the input field after adding todo
-	// 	} catch (err) {
-	// 		console.error(err.message);
-	// 	}
-	// };
-	
-
 	return (
 		<Fragment>
 			<form className='input-todo' onSubmit={onSubmitForm}>
