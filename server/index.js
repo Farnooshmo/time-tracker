fc611148bd40f45d61abbd06d7365f56ad7d7db8
Farnooshmo <91014204+Farnooshmo@users.noreply.github.com>
2024-04-15 13:34:07 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,22 @@ const cors = require('cors');
 const pool = require('./db');
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+    'https://time-tracker-team.vercel.app',
+    'http://localhost:3000' // Assuming your localhost runs on port 3000
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Create Todo
